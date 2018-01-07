c4e7ecff63c7fc4ec3dbcba8fffa23d3e9d9ca48
pedro <plazari96@gmail.com>
2018-01-07 00:11:21 -0200

    Criando uma mini lib pro Sintegra

---
+++ app/Sintegra/SintegraEs.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pedrolazari
+ * Date: 07/01/2018
+ * Time: 00:03
+ */
+
+namespace App\Sintegra;
+
+
+class SintegraEs
+{
+    public function __construct(){
+        //Buscar a URL no form do site
+        //Passar o parametro que o site deseja para poder pegar o conteúdo da página
+        //Armazenar o conteudo da página em uma variavel
+        //Aprender regiex básico para pegar tudo o que estiver dentro da sessão HTML em questão
+        //Criar um metodo para buscar cada uma das coisas, espero que o site do sintegra tenha um regex simples
+    }
+}
